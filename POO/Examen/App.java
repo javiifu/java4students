f76135914f0def0d8e042ba6e0589cd8c60b0440
Saul Moreno <predaomfg@gmail.com>
2025-03-24 12:28:04 +0100

    Fix: Main Menu

--- POO/Examen/App.java
+++ POO/Examen/App.java
@@ -13,11 +13,10 @@ public class App {
             System.out.println("2. Registrar Plato");
             System.out.println("3. Registrar Pedido");
             System.out.println("4. Modificar Pedido");
-            System.out.println("5. Borrar Pedido");
-            System.out.println("6. Modificar o Borrar Plato");
-            System.out.println("7. Guardar Datos");
-            System.out.println("8. Cargar Datos");
-            System.out.println("9. Salir");
+            System.out.println("5. Modificar o Borrar Plato");
+            System.out.println("6. Guardar Datos");
+            System.out.println("7. Cargar Datos");
+            System.out.println("8. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = scanner.nextInt();
 
@@ -27,24 +26,21 @@ public class App {
                 case 3 -> restaurante.registrarPedido();
                 case 4 -> restaurante.menuModificarPedido();
                 case 5 -> {
-                    //borrarPedido();
-                }
-                case 6 -> {
                     //modificarBorrarPlato();
                 }
-                case 7 -> {
+                case 6 -> {
                     //guardarDatos();
                 }
-                case 8 -> {
+                case 7 -> {
                     //cargarDatos();
                 }
-                case 9 -> {
+                case 8 -> {
                     //guardarDatos();
                     System.out.println("Saliendo del programa...");
                 }
                 default -> System.out.println("Opción no válida, intente nuevamente.");
             }
             
-        } while (opcion != 9);
+        } while (opcion != 8);
     }
 }
